bd51bac90ddcc2134e5a3c36bee25127165c12b8
stefan <dev@2904.cc>
2015-08-21 01:28:05 +0200

    fixed variable name in keydemo java code

--- figures/KeyDemo.java
+++ figures/KeyDemo.java
@@ -3,4 +3,4 @@ PGPReader reader = new PGPReader(in);
 PGPKeyRing keyRing = new PGPKeyRing(reader);
 
 // get keypair by User ID
-PGPKeyPair rsaRsa1024 = keyRing.getKeyPair("st <crypto@2904.cc>");
+PGPKeyPair key = keyRing.getKeyPair("st <crypto@2904.cc>");
